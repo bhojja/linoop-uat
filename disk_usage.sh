e0c0d84a1c3ba1eda98c836b8a897f11f50ed252
root <root@192.168.10.26>
2020-11-25 12:06:21 -0500

    these are test comments

---
+++ disk_usage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#
+#
+#this is our test shell script
+
+
+if [ -f /etc/hosts ]
+then
+
+        echo "i love you all" >> /etc/hosts
+
+
+else
+   
+        echo "i love you all even more" >> /etc/hosts
+
+
+
+
+
+
+
